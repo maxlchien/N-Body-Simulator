088df10335499b2935451aa5a36be133e699347f
JJ5421 <jeremykuz03@gmail.com>
2025-12-07 18:01:21 -0500

    added midpoint to test

--- src/tests/test_simulations.py
+++ src/tests/test_simulations.py
@@ -23,12 +23,19 @@ def test_two_body_orbit_euler():
 
     # Extract positions at start and end
     pos_start = states[0, :, 0:2]
+    pos_mid = states[states.shape[0] // 2, :, 0:2]
     pos_end = states[-1, :, 0:2]
 
     # Since the simulation is one full period, the positions should return to (approximately) initial
     for i in range(len(bodies)):
         assert pos_start[i][0] == pytest.approx(pos_end[i][0], abs=1e-3)
         assert pos_start[i][1] == pytest.approx(pos_end[i][1], abs=1e-3)
+
+    # Verify bodies have swapped places at t = 0.5T
+    assert pos_mid[0][0] == pytest.approx(pos_start[1][0], abs=1e-3)
+    assert pos_mid[0][1] == pytest.approx(pos_start[1][1], abs=1e-3)
+    assert pos_mid[1][0] == pytest.approx(pos_start[0][0], abs=1e-3)
+    assert pos_mid[1][1] == pytest.approx(pos_start[0][1], abs=1e-3)
 
 
 def test_two_body_orbit_barnes_hut():
@@ -45,6 +52,7 @@ def test_two_body_orbit_barnes_hut():
 
     # Extract positions at start and end
     pos_start = states[0, :, 0:2]
+    pos_mid = states[states.shape[0] // 2, :, 0:2]
     pos_end = states[-1, :, 0:2]
 
     # Since the simulation is one full period, the positions should return to (approximately) initial
@@ -52,23 +60,29 @@ def test_two_body_orbit_barnes_hut():
         assert pos_start[i][0] == pytest.approx(pos_end[i][0], abs=3e-2)
         assert pos_start[i][1] == pytest.approx(pos_end[i][1], abs=3e-2)
 
+    # Verify bodies have swapped places at t = 0.5T
+    assert pos_mid[0][0] == pytest.approx(pos_start[1][0], abs=3e-2)
+    assert pos_mid[0][1] == pytest.approx(pos_start[1][1], abs=3e-2)
+    assert pos_mid[1][0] == pytest.approx(pos_start[0][0], abs=3e-2)
+    assert pos_mid[1][1] == pytest.approx(pos_start[0][1], abs=3e-2)
+
 
 # def test_three_body_triangle_orbit():
 #     """Test a symmetric three-body equilateral triangle orbit using a known YAML config."""
-
+#
 #     config_path = Path(__file__).parent.parent / "nbody" / "config" / "triangle_test.yaml"
-
+#
 #     # Load bodies and simulation parameters
 #     bodies, sim_params = read_simulation_config(config_path)
-
+#
 #     # Run the Euler propagator
 #     propagator = EulerPropagator(bodies, sim_params)
 #     states = propagator.propagate()
-
+#
 #     # Extract positions at start and end
 #     pos_start = states[0, :, 0:2]
 #     pos_end = states[-1, :, 0:2]
-
+#
 #     # Since the simulation is one full period, the positions should return to (approximately) initial
 #     for i in range(len(bodies)):
 #         assert pos_start[i][0] == pytest.approx(pos_end[i][0], abs=1e-4)
